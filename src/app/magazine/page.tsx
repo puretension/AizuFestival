8f93ddcddd077957bf06befa0dd7ea7c6bc9b08b
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-12-12 12:13:38 +0900

    Feat: 최종본 반영

--- src/app/magazine/page.tsx
+++ src/app/magazine/page.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-
 type CustomCardProps = {
   title: string;
   description: string;
@@ -14,28 +13,30 @@ type CustomCardProps = {
 const CustomCard = ({ title, description, imageSrc, link }: CustomCardProps) => {
   return (
     <motion.div
-      className="flex flex-row bg-white rounded-xl shadow-lg overflow-hidden mb-8 hover:shadow-2xl transition-all duration-300 transform hover:-translate-y-2"
+      className="flex flex-col bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-2xl transition-all duration-300 transform hover:-translate-y-2"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <div className="w-1/2 relative">
+      {/* Image Section */}
+      <div className="relative h-78 sm:h-64 lg:h-80">
         <img
           src={imageSrc}
           alt={title}
-          className="w-full h-auto object-cover rounded-l-xl"
+          className="w-full h-full object-cover"
         />
       </div>
 
-      <div className="w-1/2 p-6 flex flex-col justify-center">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4">{title}</h2>
-        <p className="text-gray-700 leading-relaxed mb-4">{description}</p>
+      {/* Content Section */}
+      <div className="p-4 flex flex-col justify-between">
+        <h2 className="text-xl font-bold text-gray-800 mb-2">{title}</h2>
+        <p className="text-gray-700 mb-4">{description}</p>
         {link && (
           <a
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center text-blue-500 font-semibold hover:underline"
+            className="inline-block mt-auto text-blue-500 font-semibold hover:underline"
           >
             기사 보러가기
           </a>
@@ -50,19 +51,19 @@ const MagazinePage = () => {
     {
       imageSrc: 'https://www.aizukanko.com/kk/festival/wp-content/uploads/2016/01/IMG_5704.jpg',
       title: '후쿠시마 아이즈 축제의 매력',
-      description: '아이즈 축제는 동일본대지진 복구를 기원하며 후쿠시마 지역의 전통과 문화를 조명하는 대표적인 행사입니다. 초겨울의 밤하늘을 수놓는 약 만발의 불꽃이 압권입니다. 또한, 다양한 노점과 전통 공연을 통해 방문객들에게 독특한 경험을 선사합니다.',
+      description: '아이즈 축제는 동일본대지진 복구를 기원하며 후쿠시마 지역의 전통과 문화를 조명하는 대표적인 행사입니다. 초겨울의 밤하늘을 수놓는 약 만발의 불꽃이 압권입니다.',
       link: 'https://ko.japantravel.com/%ED%9B%84%EC%BF%A0%EC%8B%9C%EB%A7%88/%ED%9B%84%EC%BF%A0%EC%8B%9C%EB%A7%88%EC%9D%98-%EC%95%84%EC%9D%B4%EC%A6%88-%EC%B6%95%EC%A0%9C/64831',
     },
     {
       imageSrc: 'https://a2.cdn.japantravel.com/photo/23485-129215/800!/fukushima-aizu-festival-in-fukushima-129215.jpg',
       title: '아이즈 축제 가이드',
-      description: '아이즈와카마쓰 시는 풍부한 사무라이 유산으로 잘 알려져 있습니다. 쓰루가조 성 주변과 주요 거리에서는 매년 전통적인 퍼레이드와 다양한 문화 행사가 열리며, 사무라이 문화와 역사를 직접 체험할 수 있습니다.',
+      description: '아이즈와카마쓰 시는 풍부한 사무라이 유산으로 잘 알려져 있습니다. 쓰루가조 성 주변과 주요 거리에서는 매년 전통적인 퍼레이드와 다양한 문화 행사가 열립니다.',
       link: 'https://www.aizukanko.com/kk/festival/aizu-matsuri',
     },
     {
       imageSrc: 'https://img.cooljapan-videos.com/files/articles/8lqi7ias/thumbnail/0d9f5e3a84d2032fb750b93ef111991f.jpg',
       title: '아이즈 불꽃축제 리뷰',
-      description: 'Cool Japan에서 소개한 아이즈 불꽃축제는 동일본대지진 복구를 기원하며 약 만발의 불꽃이 후쿠시마 밤하늘을 수놓는 장관을 연출합니다. 일본 불꽃 장인들이 참여해 예술적인 불꽃 연출을 선보입니다.',
+      description: 'Cool Japan에서 소개한 아이즈 불꽃축제는 동일본대지진 복구를 기원하며 약 만발의 불꽃이 후쿠시마 밤하늘을 수놓는 장관을 연출합니다.',
       link: 'https://cooljapan-videos.com/ko/articles/8lqi7ias',
     },
   ];
--- src/components/home/banner/CardComponents.tsx
+++ src/components/home/banner/CardComponents.tsx
@@ -7,23 +7,28 @@ export const CardComponents = () => {
   const cards = [
     {
       title: 'AIZU',
-      subtitle: 'about state',
-      content:
-        'Body text for your whole article or post. We\'ll put in some lorem ipsum to show how a filled-out page might look: Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content.',
-      imageSrc: '/images/aizu_castle_default.jpg',
+      subtitle: '아이즈 지역 소개',
+      content: `아이즈(会津)는 후쿠시마의 세 지역 중 가장 서쪽에 위치한 지역입니다. 아이즈의 주요 도시는 츠루가 성으로 유명한 아이즈와카마츠입니다.
+이곳에서는 에도 시대 말기의 역사적 배경과 관련된 깊이 뿌리내린 전통과 사무라이 문화를 볼 수 있습니다. 또한, 일본에서도 유명한 풍부한 자연환경을 자랑합니다.
+아이즈 지역은 사방이 산으로 둘러싸여 있으며, 오우산맥 서쪽에 위치하여 동해 지역 특유의 기후를 나타냅니다. 또한 이 지역은 폭설 지대로도 잘 알려져 있습니다.
+이렇듯, 아이즈 지역은 매우 매력적인 지역입니다.
+`,
+      imageSrc:'/images/aizu_castle_default.jpg',
     },
     {
       title: 'AIZU FESTIVAL',
-      subtitle: 'historical background',
-      content:
-        'Explore the rich history of Aizu Festival through a carefully curated experience that brings together cultural elements. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content.',
+      subtitle: '역사적 배경',
+      content: `아이즈와카마츠는 사무라이 유산으로 일본에서도 널리 알려져 있습니다. 쓰루가조 성은 보신 전쟁 때 막부에 충성을 마지막까지 다하며 메이지 신 정부에게 ‘아침의 적’으로 여겨졌습니다. 이들은 마지막까지 막부에 충성하였으며, 유명한 백호대의 비극극도 아이즈에서 일어난 일입니다.
+그리고 약 60년 후, 일본 황실과 아이즈의 영주의 손녀가 결혼하면서 황실에서 아이즈는 아침의 적이 아님을 공표 해줌으로써 아이즈는 일본 정부와 화해했습니다. 아이즈 주민들은 이 결혼을 기념하며 아이즈의 회복을 가능하게 한 ‘축하와 기쁨’의 정신을 후대에 전하기 위해 아이즈 축제를 시작하게 되었습니다.
+`,
       imageSrc: '/images/aizu_castle_day.png',
     },
     {
       title: 'NOW',
-      subtitle: 'recover',
-      content:
-        'Dive into the revival and preservation of Aizu\'s traditions, as we showcase how the community is recovering and embracing its future. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content.',
+      subtitle: '회복된 현재상태',
+      content: `후쿠시마 원전 사고 이후, 아이즈 지역은 회복과 미래로의 발전, 그리고 융화를 향한 여정을 시작했습니다. 역사적 갈등 이후 아이즈의 화합과 재건을 기념하며 시작된 아이즈 축제는 세대를 이어온 축하와 기쁨의 정신을 담고 있습니다.
+      현대사회에서 이 축제는 새로운 의의를 가지게 되었습니다. 아이즈 주민들의 회복력과 끈질긴 정신을 기리는 것은 물론, 역경을 극복하고 더 밝은 미래를 향해 나아가려는 지역의 의지를 상징합니다. 아이즈 축제는 이제 단순히 지역의 전통과 문화를 기리는 것을 넘어, 지역사회의 연대와 희망, 그리고 공동체 정신을 기념하는 자리로 자리매김했습니다.
+      아이즈 축제는 역사적 뿌리를 현재의 비전과 연결하며, 극복과 재생의 정신을 지속적으로 보여줍니다. 이는 지역 주민뿐만 아니라 방문객들에게도 강렬한 희망과 연대의 메시지를 전달하는 상징적 행사로 자리하고 있습니다.`,
       imageSrc: '/images/aizu_castle_night.png',
     },
   ];
